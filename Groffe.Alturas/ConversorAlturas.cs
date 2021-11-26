19a6d95f950d8a3ccd567f75a32db10e234d9d23
Renato Groffe <renato.groffe@yahoo.com.br>
2021-11-26 19:58:18 -0300

    Commit inicial
    Commit inicial

---
+++ Groffe.Alturas/ConversorAlturas.cs
@@ -0,0 +1,14 @@
+﻿namespace Groffe.Alturas;
+
+public class ConversorAlturas
+{
+    /// <summary>
+    /// Converte uma altura em pes para o equivalente em metros
+    /// </summary>
+    /// <param name="alturaPes">Altura em pes</param>
+    /// <returns></returns>
+    public static double PesParaMetros(double alturaPes)
+    {
+        return Math.Round(alturaPes * 0.3048, 4);
+    }
+}
