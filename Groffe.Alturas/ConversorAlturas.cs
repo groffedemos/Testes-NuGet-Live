d73685114b970451e1a44888c8bafe3f03211dc6
Renato Groffe <renato.groffe@yahoo.com.br>
2021-11-26 21:02:03 -0300

    Ajustes validando altura v1.0.1

--- Groffe.Alturas/ConversorAlturas.cs
+++ Groffe.Alturas/ConversorAlturas.cs
@@ -6,9 +6,12 @@ public class ConversorAlturas
     /// Converte uma altura em pes para o equivalente em metros
     /// </summary>
     /// <param name="alturaPes">Altura em pes</param>
-    /// <returns></returns>
+    /// <returns>Valor em metros</returns>
+    /// <exception cref="ArgumentException">Exceção gerada para uma altura negativa</exception>
     public static double PesParaMetros(double alturaPes)
     {
+        if (alturaPes <= 0)
+            throw new ArgumentException("Altura em pes deve ser maior do que zero!");
         return Math.Round(alturaPes * 0.3048, 4);
     }
 }
